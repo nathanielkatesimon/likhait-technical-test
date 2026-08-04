d15011a7d365f035cca2519097fc46580503b26a
nathanielkatesimon <nathanielkatesimon@gmail.com>
2026-08-05 01:10:15 +0800

    [FEATURE] Revert ordering to alphabetical order

--- backend/app/controllers/api/categories_controller.rb
+++ backend/app/controllers/api/categories_controller.rb
@@ -1,6 +1,6 @@
 class Api::CategoriesController < ApplicationController
   def index
-    categories = Category.order(created_at: :desc)
+    categories = Category.order(:name)
     render json: categories
   end
 
