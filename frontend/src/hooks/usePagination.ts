599bf3a20e9d781bb632133523c8f08e80292b6f
nathanielkatesimon <nathanielkatesimon@gmail.com>
2026-08-05 00:25:23 +0800

    [REFACTOR] set page to 1 if a new item is added

--- frontend/src/hooks/usePagination.ts
+++ frontend/src/hooks/usePagination.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export function usePagination(items: any[], items_per_page: number) {
   const [currentPage, setCurrentPage] = useState(1);
+  const [itemsCount, setItemsCount] = useState(items.length);
 
   const totalPages = Math.ceil(items.length / items_per_page);
   const startIndex = (currentPage - 1) * items_per_page;
@@ -11,6 +12,11 @@ export function usePagination(items: any[], items_per_page: number) {
   useEffect(() => {
     if(currentItems.length === 0){
       setCurrentPage(currentPage > 1 ? currentPage - 1 : 1);
+    }
+
+    if(itemsCount < items.length){
+      setItemsCount(items.length);
+      setCurrentPage(1);
     }
   }, [items]);
 
